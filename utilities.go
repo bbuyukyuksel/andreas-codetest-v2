25dc134b08b515cbcb468afbc8ff7b654bc7fcef
Burak Büyükyüksel <buyukyukselburak@gmail.com>
2022-05-27 13:29:33 +0300

    Append comment on utilities file, each function has been commented on what it does

--- utilities.go
+++ utilities.go
@@ -3,5 +3,6 @@ package main
 import "time"
 
 func TimeWithOffset(offset int, ctime time.Time) time.Time {
+	// Add offset to time.Time object
 	return ctime.Add(time.Duration(offset) * time.Second)
 }
